ed200421e60ba671c5528ab08da1e9fe7f942df3
EC2 Default User <kazukipump7@gmail.com>
2022-09-02 17:37:57 +0000

    mouseover

--- app.js
+++ app.js
@@ -61,14 +61,14 @@
 //   });
 // });
 
-$(function(){
-  $('.box1').mouseover(function(){
-    $('.box1').css({'background-color': '#0000FF'});
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').css({'background-color': '#FF0000'});
-  });
-});
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
 
 // $(function(){
 //   $('.box1').mouseover(function(){
@@ -78,3 +78,12 @@ $(function(){
 //     $('.box1').css({'background-color': '#FF0000'});
 //   });
 // });
+
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+});
