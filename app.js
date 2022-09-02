d7d27cbb750481ef5f35e783d9bf1ed999062585
EC2 Default User <kazukipump7@gmail.com>
2022-09-02 13:51:21 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('body').html('<h1>Hello jQuery!!<h1>');
+});
+
+$(document).ready(function(){
+  //jQueryプログラムnの内容
+});
+
+// 頻繁に使うコードなので、省略形も用意されています。
+
+$(function(){
+  //jQuery(プログラムの内容)
+});
