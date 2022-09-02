b1cea44bad130e29d9ddf1bd3405d687281fd4ad
EC2 Default User <kazukipump7@gmail.com>
2022-09-02 14:59:04 +0000

    second commit

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-$(document).ready(function(){
-  $('body').html('<h1>Hello jQuery!!<h1>');
+// $(document).ready(function(){
+//   $('body').html('<h1>Hello jQuery!!<h1>');
+// });
+
+// // $(document).ready(function(){
+// //   //jQueryプログラムnの内容
+// // });
+
+// // // 頻繁に使うコードなので、省略形も用意されています。
+
+// // $(function(){
+// //   //jQuery(プログラムの内容)
+// // });
+// $('body').html('<h1>Hello JQuery!!(/h1)');
+
+$(function(){
+  $('.box1').css({
+    'background-color': '#0000FF',
+    'height': '100px'
+  });
 });
 
-// $(document).ready(function(){
-//   //jQueryプログラムnの内容
-// });
 
-// // 頻繁に使うコードなので、省略形も用意されています。
-
-// $(function(){
-//   //jQuery(プログラムの内容)
-// });
-$('body').html('<h1>Hello JQuery!!(/h1)');
